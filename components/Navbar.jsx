3843765887387a83ab6e91a5881f3dfe035ec338
Tonmoy01 <mdtonmoykhan65@gmail.com>
2024-05-18 21:06:30 +0600

    complete next auth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,8 +28,6 @@ export default function Navbar() {
     setAuthProviders();
   }, []);
 
-  console.log(session);
-
   return (
     <nav className='bg-blue-700 border-b border-blue-500'>
       <div className='px-2 mx-auto max-w-7xl sm:px-6 lg:px-8'>
@@ -193,6 +191,9 @@ export default function Navbar() {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-0'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Your Profile
                     </Link>
@@ -202,10 +203,17 @@ export default function Navbar() {
                       role='menuitem'
                       tabIndex='-1'
                       id='user-menu-item-2'
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                      }}
                     >
                       Saved Properties
                     </Link>
                     <button
+                      onClick={() => {
+                        setIsProfileMenuOpen(false);
+                        signOut();
+                      }}
                       className='block px-4 py-2 text-sm text-gray-700'
                       role='menuitem'
                       tabIndex='-1'
