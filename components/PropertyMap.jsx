e5d6bc6ba10e2962f405a2215d669eeaba2b96ec
Tonmoy01 <mdtonmoykhan65@gmail.com>
2024-05-30 20:33:49 +0600

    make search form component

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,54 +1,43 @@
 'use client';
-import { useEffect, useState } from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import Map, { Marker } from 'react-map-gl';
-import { setDefaults, fromAddress } from 'react-geocode';
+
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 import Spinner from './Spinner';
-import Image from 'next/image';
 import pin from '@/assets/images/pin.svg';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: pin,
+  iconUrl: pin,
+  shadowUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
+});
 
 export default function PropertyMap({ property }) {
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
   const [loading, setLoading] = useState(true);
   const [geocodeError, setGeocodeError] = useState(false);
-  const [viewPort, setViewPort] = useState({
-    lattitude: 0,
-    longitude: 0,
-    zoom: 12,
-    width: '100%',
-    height: '500px',
-  });
-
-  setDefaults({
-    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
-    language: 'en',
-    region: 'us',
-  });
 
   useEffect(() => {
     const fetchCoords = async () => {
       try {
-        const res = await fromAddress(
-          `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
+        const res = await fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${property.location.street},${property.location.city},${property.location.state},${property.location.zipcode}&key=${process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY}`
         );
+        const data = await res.json();
 
-        if (res.results.length === 0) {
+        if (data.results.length === 0) {
           setGeocodeError(true);
           setLoading(false);
           return;
         }
 
-        const { lat, lng } = res.results[0].geometry.location;
-
+        const { lat, lng } = data.results[0].geometry.location;
         setLat(lat);
         setLng(lng);
-        setViewPort({
-          ...viewPort,
-          lattitude: lat,
-          longitude: lng,
-        });
-
         setLoading(false);
       } catch (error) {
         console.log(error);
@@ -58,7 +47,7 @@ export default function PropertyMap({ property }) {
     };
 
     fetchCoords();
-  }, []);
+  }, [property.location]);
 
   if (loading) return <Spinner loading={loading} />;
 
@@ -67,22 +56,21 @@ export default function PropertyMap({ property }) {
   }
 
   return (
-    !loading && (
-      <Map
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-        mapLib={import('mapbox-gl')}
-        initialViewState={{
-          longitude: lng,
-          latitude: lat,
-          zoom: 15,
-        }}
-        style={{ width: '100%', height: 500 }}
-        mapStyle='mapbox://styles/mapbox/street-v9'
-      >
-        <Marker longitude={lng} latitude={lat} anchor='bottom'>
-          <Image src={pin} alt='location' width={40} height={40} />
-        </Marker>
-      </Map>
-    )
+    <MapContainer
+      center={[lat, lng]}
+      zoom={13}
+      style={{ height: '500px', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+      />
+      <Marker position={[lat, lng]}>
+        <Popup>
+          {property.location.street}, {property.location.city},{' '}
+          {property.location.state}
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
